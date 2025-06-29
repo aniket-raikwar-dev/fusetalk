59dc013b1eb6d49628e401fe864c5ee2c9680d41
aniket-dev <aniket.raikwar.101@gmail.com>
2025-06-29 10:08:32 +0530

    font color change

--- frontend/src/pages/SplashScreen.jsx
+++ frontend/src/pages/SplashScreen.jsx
@@ -63,15 +63,13 @@ const SplashScreen = () => {
       </p>
 
       <button className="splash-btn">
-        Contibue With Us{" "}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="#000"
-        >
+        Join Now{" "}
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#000">
           <path d="M1.99974 13.0001L1.9996 11.0002L18.1715 11.0002L14.2218 7.05044L15.636 5.63623L22 12.0002L15.636 18.3642L14.2218 16.9499L18.1716 13.0002L1.99974 13.0001Z"></path>
         </svg>
       </button>
+
+      <p className="copyright">ⓒ all rights reserved to fusetalk.</p>
     </div>
   );
 };
